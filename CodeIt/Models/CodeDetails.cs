30ece81f3cc09330ab930f2a8a79ab3ed3cd8a47
Viktor <viktor.mirev@gmail.com>
2017-04-22 22:41:57 +0300

    commit

---
+++ CodeIt/Models/CodeDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeIt.Models
+{
+    public class CodeDetails
+    {
+       
+        [Display(Name = "Code Title")]
+        public string CodeTitle { get; set; }
+
+        [Display(Name = "Code")]
+        public List<string> CodeContent { get; set; }
+
+        [Display(Name = "Author")]
+        public string Author{ get; set; }
+
+
+        public List<string> Comments { get; set; }
+
+
+        public string TabSpammer(int n)
+        {
+            string res = "";
+            for(int i = 0; i < n; i++)
+            {
+                res += "&nbsp; ";
+            }
+            return res;
+        }
+    }
+}
---
+++ CodeIt/Models/CreateCodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeIt.Models
+{
+    public class CreateCodeModel
+    {
+        [Required]
+        [Display(Name = "Code Title")]
+        [StringLength(200)]
+        public string CodeTitle { get; set; }
+
+        [Required]
+        public string CodeContent { get; set; }
+
+
+    }
+}
